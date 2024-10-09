64ddc5688c347014127ca3d2c8fbb3f1568310a9
tequila01-svg <onifadeayobami008@gmail.com>
2024-10-09 16:11:55 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+first_name="Ayobami"
+Last_name="Onifade"
+full_name=Last_name+first_name
+example="nice"*6
+
+print("first_name:" , first_name)
+print("Last_name:" , Last_name)
+print("full_name:" , full_name)
+print("string multiplied by 6 to give this result:" , example)
+
+word="Ayobami"
+print("length of string:", len(word))
+print("first letter of string:", word[0])
+print("last letter of string:", word[6])
+print("string sliced:", word[0:4])
